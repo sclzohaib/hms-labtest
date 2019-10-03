004ba53a385b84c8f14e3e476402514e6768d926
Muhammad-Amer1997 <amer.rasool121@gmail.com>
2019-10-04 00:15:51 +0500

    url fixed

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   
   production: false,
   tokenURL: 'http://localhost:8080/',
-  baseUrl: 'http://localhost:8081/',
+  baseurl: 'http://localhost:8081/',
   opdUrl: 'http://localhost:8080/',
   labUrl: 'http://localhost:8081/',
   pharmacyUrl: 'http://localhost:8082/'
